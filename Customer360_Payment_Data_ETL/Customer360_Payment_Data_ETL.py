f8a90eb46d851c985cad4ba99ef6768e2e3eb885
harini-spec <ponshriharini@gmail.com>
2024-11-25 16:18:28 +0530

    Payment Data - ID logic removed

--- Customer360_Payment_Data_ETL/Customer360_Payment_Data_ETL.py
+++ Customer360_Payment_Data_ETL/Customer360_Payment_Data_ETL.py
@@ -3,19 +3,16 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-from pyspark.sql.functions import lit, when, expr
-import pyspark.sql.functions as F
+from pyspark.sql.functions import lit, when
 from awsglue.dynamicframe import DynamicFrame
 import time
 import boto3
 from botocore.exceptions import ClientError
 import logging
-import pytz
 from datetime import datetime
 import json
 import concurrent.futures
-from pyspark.sql.window import Window
-from pyspark.sql.functions import row_number
+from pyspark.sql.functions import current_timestamp
 
 # Setup logging
 logging.basicConfig(level=logging.INFO)
@@ -43,7 +40,7 @@ def initialize_glue_job():
     return glue_context, job
 
 # Load Redshift credentials from AWS Secrets Manager
-def get_secret(region_name="us-east-1"):
+def get_secret():
     """
     Retrieve Redshift credentials from AWS Secrets Manager.
     """
@@ -53,6 +50,9 @@ def get_secret(region_name="us-east-1"):
         # Resolve job parameters
         args = getResolvedOptions(sys.argv, ['SECRET_NAME'])
         secret_name = args['SECRET_NAME']
+        
+        args = getResolvedOptions(sys.argv, ['REGION_NAME'])
+        region_name = args['REGION_NAME']
         
         # Create a Secrets Manager client
         session = boto3.session.Session()
@@ -83,7 +83,7 @@ def load_data_from_s3(glue_context, s3_source_path):
                 "jobBookmarkKeys": ["filename"], 
                 "jobBookmarkKeysSortOrder": "asc"
             },
-            transformation_ctx=f"Redshift_job_bookmark_payment_details",
+            transformation_ctx=f"Redshift_jb_payment_details",
             format="csv",
             format_options={"withHeader": True, "separator": ",", "quoteChar": "\""}
         )
@@ -91,36 +91,9 @@ def load_data_from_s3(glue_context, s3_source_path):
     except Exception as e:
         logger.error(f"Error loading data from S3: {e}")
         raise
-    
-# Fetch the last payment_id from Redshift
-def get_last_payment_id_from_redshift(glue_context, connection_options, table_name):
-    """
-    Fetch the last payment_id from Redshift, and handle the case where the table does not exist.
-    """
-    try:
-        logger.info(f"Fetching last payment_id from Redshift table: {table_name}...")
-        
-        # Create a Spark session for querying Redshift
-        redshift_df = glue_context.read.format("jdbc").options(
-            url=connection_options["url"],
-            dbtable=f"public.{table_name}",
-            user=connection_options["user"],
-            password=connection_options["password"]
-        ).load()
-        
-        # Get the max payment_id from the table
-        last_payment_id = redshift_df.agg({"payment_id": "max"}).collect()[0][0]
-        
-        return last_payment_id if last_payment_id is not None else 0  # Return 0 if no payment_id exists
-    except Exception as e:
-        logger.error(f"Error fetching last payment_id from Redshift: {e}")
-        
-        # Handle table not found error or any other exceptions by setting last_payment_id to 0
-        logger.info(f"Table not found or error occurred, defaulting last_payment_id to 0.")
-        return 0
 
 # Transform the S3 data
-def transform_s3_data(s3_data_dyf, glue_context, last_payment_id):
+def transform_s3_data(s3_data_dyf, glue_context):
     """
     Transform and map the S3 data DynamicFrame.
     """
@@ -133,31 +106,13 @@ def transform_s3_data(s3_data_dyf, glue_context, last_payment_id):
             .apply_mapping([
                 ("customer_id", "string", "customer_id", "string"),
                 ("paperless_billing", "string", "paperless_billing", "boolean"),
-                ("monthly_charges", "string", "monthly_charges", "double"),
-                ("total_charges", "string", "total_charges", "double"),
+                ("monthly_charges", "string", "monthly_charges", "decimal(10,2)"),
+                ("total_charges", "string", "total_charges", "decimal(10,2)"),
                 ("PaymentMethod", "string", "PaymentMethod", "string")
             ])
-            
-        ist_timezone = pytz.timezone("Asia/Kolkata")
-        ist_time = datetime.now(ist_timezone).strftime("%Y-%m-%d %H:%M:%S")
-        transformed_dyf = transformed_dyf.map(lambda record: {
-            **record,
-            "defaulter": False,
-            "updated_on": ist_time
-        })
-        
-        # Convert DynamicFrame to DataFrame for transformation
-        transformed_df = transformed_dyf.toDF()
-
-        # Add a self-incrementing column `payment_id`
-        window_spec = Window.orderBy(F.lit(1))  
-        
-        transformed_df = transformed_df.withColumn(
-            "payment_id", F.row_number().over(window_spec) + last_payment_id
-        )
-        
-        transformed_dyf = DynamicFrame.fromDF(transformed_df, glue_context, "transformed_data")
-            
+        df = transformed_dyf.toDF()
+        df = df.withColumn("updated_on", current_timestamp())
+        transformed_dyf = DynamicFrame.fromDF(df, glue_context, "joined_dyf_with_updated_on")
         return transformed_dyf
     except Exception as e:
         logger.error(f"Error transforming S3 data: {e}")
@@ -170,14 +125,25 @@ def load_payment_method_table(glue_context, connection_options, table_name):
     """
     try:
         logger.info("Loading payment method data from Redshift...")
+        
+        # Add Snappy compression options
+        compression_options = {
+            "compression": "snappy"
+        }
+        # Merge compression options with existing Redshift options
+        merged_connection_options = {
+            **connection_options,  # Unpack original Redshift options
+            **compression_options  # Add Snappy compression
+        }
+        
         payment_method_dyf = glue_context.create_dynamic_frame.from_options(
             connection_type="redshift",
             connection_options={
-                "url": connection_options["url"],
+                "url": merged_connection_options["url"],
                 "dbtable": table_name,
-                "user": connection_options["user"],
-                "password": connection_options["password"],
-                "redshiftTmpDir": connection_options["redshiftTmpDir"]
+                "user": merged_connection_options["user"],
+                "password": merged_connection_options["password"],
+                "redshiftTmpDir": merged_connection_options["redshiftTmpDir"]
             }
         )
         return payment_method_dyf
@@ -188,7 +154,7 @@ def load_payment_method_table(glue_context, connection_options, table_name):
 # Join S3 data with payment method data
 def join_with_payment_method_id(s3_data_dyf, payment_method_dyf):
     """
-    Perform join operation with payment method table and update timestamp.
+    Perform join operation with payment method table.
     """
     try:
         logger.info("Joining S3 data with payment method data...")
@@ -196,7 +162,10 @@ def join_with_payment_method_id(s3_data_dyf, payment_method_dyf):
             paths1=["PaymentMethod"], paths2=["payment_method"], frame2=payment_method_dyf
         )
         
-        joined_data_dyf = joined_data_dyf.drop_fields(["payment_method", "PaymentMethod"])
+        joined_data_dyf = joined_data_dyf.map(lambda record: {
+            **record,
+            "defaulter": False
+        }).drop_fields(["payment_method", "PaymentMethod"])
         
         return joined_data_dyf
     except Exception as e:
@@ -210,19 +179,19 @@ def write_to_redshift(glue_context, dyf, connection_options, table_name):
     """
     try:
         logger.info(f"Writing data to Redshift table: payment_details...")
+        options = {
+            **connection_options,
+            "dbtable": f"public.{table_name}"
+        }
+        
+        # Perform the write operation
         glue_context.write_dynamic_frame.from_options(
             frame=dyf,
             connection_type="redshift",
-            connection_options={
-                "url": connection_options["url"],
-                "dbtable": f"public.{table_name}",
-                "user": connection_options["user"],
-                "password": connection_options["password"],
-                "redshiftTmpDir": connection_options["redshiftTmpDir"],
-                "connectionName": connection_options["connectionName"]
-            },
-            transformation_ctx=f"Redshift_job_bookmark_payment_details"
+            connection_options=options,
+            transformation_ctx="Redshift_jb_payment_details"
         )
+
     except Exception as e:
         logger.error(f"Error writing data to Redshift: {e}")
         raise
@@ -240,9 +209,13 @@ def main():
         glue_context, job = initialize_glue_job()
         log_time_taken(initialize_start, "Job Initialization")
 
-        # Global configurations
-        s3_source_path = "s3://customer360-v1-s3bucket/raw/year=2024/"
-        redshift_tmp_dir = "s3://aws-glue-assets-894811220469-us-east-1/temporary/"
+        # Global configurations -- Resolve job parameters
+        args = getResolvedOptions(sys.argv, ['S3_SOURCE_PATH'])
+        s3_source_path = args['S3_SOURCE_PATH']
+        
+        args = getResolvedOptions(sys.argv, ['REDSHIFT_TEMP_DIR'])
+        redshift_tmp_dir = args['REDSHIFT_TEMP_DIR']
+
         payment_method_table = "public.dim_payment_method"
 
         # Fetch Redshift credentials
@@ -255,25 +228,22 @@ def main():
         s3_data_dyf = load_data_from_s3(glue_context, s3_source_path)
         log_time_taken(read_from_s3_start, "Reading S3 Data")
         
-        # Step 2: Load Payment Method Data from Redshift
+        # Step 2: Read from S3 
         read_from_redshift_start = time.time()
         payment_method_dyf = load_payment_method_table(glue_context, redshift_connection_options, payment_method_table)
         log_time_taken(read_from_redshift_start, "Reading Redshift Data")
-        
-        # Step 3: Get last Id
-        last_payment_id = get_last_payment_id_from_redshift(glue_context, redshift_connection_options, "payment_details")
 
-        # Step 4: Transform S3 data
+        # Step 3: Transform S3 data
         transform_start = time.time()
-        s3_data_dyf = transform_s3_data(s3_data_dyf, glue_context, last_payment_id)
+        s3_data_dyf = transform_s3_data(s3_data_dyf, glue_context)
         log_time_taken(transform_start, "Transforming S3 Data")
 
-        # Step 5: Join S3 data with payment method
+        # Step 4: Join S3 data with payment method
         join_start = time.time()
         joined_dyf = join_with_payment_method_id(s3_data_dyf, payment_method_dyf)
         log_time_taken(join_start, "Joining S3 Data with Payment Method Data")
 
-        # Step 6: Write data to Redshift
+        # Step 5: Write data to Redshift
         write_start = time.time()
         write_to_redshift(glue_context, joined_dyf, redshift_connection_options, "payment_details")
         log_time_taken(write_start, "Writing Data to Redshift")
